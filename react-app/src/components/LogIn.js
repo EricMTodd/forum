4d2050644fa59602671a3eed899fa0f998162616
Eric Todd <39493826+EricMTodd@users.noreply.github.com>
2022-10-30 09:55:05 -0600

    Formatting and styling changes.

--- react-app/src/components/LogIn.js
+++ react-app/src/components/LogIn.js
@@ -6,10 +6,43 @@ const LogIn = ({
   domain,
   logIn
 }) => {
+  // STATE AND VARIABLES
   const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  // STYLES
+  const containerStyles = {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center'
+  }
+
+  const formStyles = {
+    backgroundColor: 'rgba(100, 100, 100, 1)',
+    marginTop: '50px',
+    width: '40%',
+    padding: '50px',
+    height: 'fit-content',
+    borderRadius: '15px'
+  }
+
+  const inputStyles = {
+    borderRadius: '5px',
+    width: '100%',
+    height: '25px'
+  }
+
+  const buttonStyles = {
+    width: '101.5%',
+    height: '40px',
+    marginTop: '10px',
+    borderRadius: '25px',
+    backgroundColor: 'rgba(150, 150, 150, 1)',
+    border: 'none'
+  }
+
+  // METHODS
   const handleSubmit = e => {
     e.preventDefault()
     axios.post(`${domain}/sessions/create`, {
@@ -18,30 +51,52 @@ const LogIn = ({
     })
     .then(response => {
       logIn(response.data)
-      navigate('/')
+      if (response.data.logged_in) {
+        navigate('/')
+      }
     })
     .catch(error => console.log(error))
   }
 
+  const buttonHoverEffect = e => {
+    if (e.target.style.backgroundColor !== 'blue') {
+      e.target.style.backgroundColor = 'blue'
+      e.target.style.color = 'white'
+    } else {
+      e.target.style.backgroundColor = 'rgba(150, 150, 150, 1)'
+      e.target.style.color = 'black'
+    }
+  }
+
+  const buttonClickEffect = e => {
+    if (e.target.style.backgroundColor !== 'white') {
+      e.target.style.backgroundColor = 'white'
+      e.target.style.color = 'black'
+    } else {
+      e.target.style.backgroundColor = 'blue'
+      e.target.style.color = 'white'
+    }
+  }
+
   return(
-    <div id='log-in'>
-      <form onSubmit={handleSubmit}>
+    <div id='log-in-form-container' style={containerStyles}>
+      <form onSubmit={handleSubmit} style={formStyles}>
         <h1 style={{marginTop: '0'}}>Log in</h1>
         <label htmlFor='log-in-email-input'>
           <strong>Email</strong>
         </label>
         <br />
-        <input type='email' id='log-in-email-input' name='email' value={email} onChange={e => setEmail(e.target.value)} />
+        <input type='email' id='log-in-email-input' name='email' value={email} onChange={e => setEmail(e.target.value)} style={inputStyles} />
         <br />
         <br />
         <label htmlFor='log-in-password-input'>
           <strong>Password</strong>
         </label>
         <br />
-        <input type='password' id='log-in-password-input' name='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <input type='password' id='log-in-password-input' name='password' value={password} onChange={e => setPassword(e.target.value)} style={inputStyles} />
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' style={buttonStyles} onMouseEnter={buttonHoverEffect} onMouseLeave={buttonHoverEffect} onMouseDown={buttonClickEffect} onMouseUp={buttonClickEffect}>Submit</button>
       </form>
     </div>
   )
--- react-app/src/components/SignUp.js
+++ react-app/src/components/SignUp.js
@@ -6,12 +6,45 @@ const SignUp = ({
   domain,
   logIn
 }) => {
+  // STATE AND VARIABLES
   const navigate = useNavigate()
   const [handle, setHandle] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
+  // STYLES
+  const containerStyles = {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center'
+  }
+
+  const formStyles = {
+    backgroundColor: 'rgba(100, 100, 100, 1)',
+    marginTop: '50px',
+    width: '40%',
+    padding: '50px',
+    height: 'fit-content',
+    borderRadius: '15px'
+  }
+
+  const inputStyles = {
+    borderRadius: '5px',
+    width: '100%',
+    height: '25px'
+  }
+
+  const buttonStyles = {
+    width: '101.5%',
+    height: '40px',
+    marginTop: '10px',
+    borderRadius: '25px',
+    backgroundColor: 'rgba(150, 150, 150, 1)',
+    border: 'none'
+  }
+
+  // METHODS
   const handleSubmit = e => {
     e.preventDefault()
     axios.post(`${domain}/users/create`, {
@@ -24,44 +57,66 @@ const SignUp = ({
     })
     .then(response => {
       logIn(response.data)
-      navigate('/')
+      if (response.data.logged_in) {
+        navigate('/')
+      }
     })
     .catch(error => console.log(error))
   }
 
+  const buttonHoverEffect = e => {
+    if (e.target.style.backgroundColor !== 'blue') {
+      e.target.style.backgroundColor = 'blue'
+      e.target.style.color = 'white'
+    } else {
+      e.target.style.backgroundColor = 'rgba(150, 150, 150, 1)'
+      e.target.style.color = 'black'
+    }
+  }
+
+  const buttonClickEffect = e => {
+    if (e.target.style.backgroundColor !== 'white') {
+      e.target.style.backgroundColor = 'white'
+      e.target.style.color = 'black'
+    } else {
+      e.target.style.backgroundColor = 'blue'
+      e.target.style.color = 'white'
+    }
+  }
+
   return(
-    <div id='sign-up-form-container'>
-      <form onSubmit={handleSubmit}>
+    <div id='sign-up-form-container' style={containerStyles}>
+      <form onSubmit={handleSubmit} style={formStyles}>
         <h1 style={{marginTop: '0'}}>Sign up</h1>
         <label htmlFor='sign-up-handle-input'>
           <strong>Handle</strong>
         </label>
         <br />
-        <input type='text' id='sign-up-handle-input' name='handle' value={handle} onChange={e => setHandle(e.target.value)} />
+        <input type='text' id='sign-up-handle-input' name='handle' value={handle} onChange={e => setHandle(e.target.value)} style={inputStyles} />
         <br />
         <br />
         <label htmlFor='sign-up-email-input'>
           <strong>Email</strong>
         </label>
         <br />
-        <input type='email' id='sign-up-email-input' name='email' value={email} onChange={e => setEmail(e.target.value)} />
+        <input type='email' id='sign-up-email-input' name='email' value={email} onChange={e => setEmail(e.target.value)} style={inputStyles} />
         <br />
         <br />
         <label htmlFor='sign-up-password-input'>
           <strong>Password</strong>
         </label>
         <br />
-        <input type='password' id='sign-up-password-input' name='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <input type='password' id='sign-up-password-input' name='password' value={password} onChange={e => setPassword(e.target.value)} style={inputStyles} />
         <br />
         <br />
         <label htmlFor='sign-up-password-confirmation-input'>
           <strong>Password confirmation</strong>
         </label>
         <br />
-        <input type='password' id='sign-up-password-confirmation-input' name='passwordConfirmation' value={passwordConfirmation} onChange={e => setPasswordConfirmation(e.target.value)} />
+        <input type='password' id='sign-up-password-confirmation-input' name='passwordConfirmation' value={passwordConfirmation} onChange={e => setPasswordConfirmation(e.target.value)} style={inputStyles} />
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' style={buttonStyles} onMouseEnter={buttonHoverEffect} onMouseLeave={buttonHoverEffect} onMouseDown={buttonClickEffect} onMouseUp={buttonClickEffect}>Submit</button>
       </form>
     </div>
   )
