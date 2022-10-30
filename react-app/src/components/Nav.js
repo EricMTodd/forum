943379187028d0c930744b8edfcaca8f627e2fd0
Eric Todd <39493826+EricMTodd@users.noreply.github.com>
2022-10-30 10:06:13 -0600

    Some styling changes.

--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -5,6 +5,7 @@ const Nav = ({
   currentUser,
   logOut
 }) => {
+
   const navStyles = {
     backgroundColor: 'black',
     height: '50px',
@@ -14,18 +15,48 @@ const Nav = ({
     padding: '0 15px 0 15px'
   }
 
+  const buttonStyles = {
+    backgroundColor: 'rgba(150, 150, 150, 1)',
+    color: 'white',
+    border: 'none',
+    fontSize: '16px',
+    padding: '5px 20px 5px 20px',
+    borderRadius: '25px',
+    textDecoration: 'none'
+  }
+
+  const buttonHoverEffect = e => {
+    if (e.target.style.backgroundColor !== 'blue') {
+      e.target.style.backgroundColor = 'blue'
+      e.target.style.color = 'white'
+    } else {
+      e.target.style.backgroundColor = 'rgba(150, 150, 150, 1)'
+      e.target.style.color = 'black'
+    }
+  }
+
+  const buttonClickEffect = e => {
+    if (e.target.style.backgroundColor !== 'white') {
+      e.target.style.backgroundColor = 'white'
+      e.target.style.color = 'black'
+    } else {
+      e.target.style.backgroundColor = 'blue'
+      e.target.style.color = 'white'
+    }
+  }
+
   let sessionsLinks
   if (loggedIn) {
     sessionsLinks =
-    <div id='sessions-links' style={{display: 'flex', gap: '5px'}}>
+    <div id='sessions-links' style={{display: 'flex', gap: '15px'}}>
       <Link to={`users/${currentUser.id}`}>{currentUser.handle}</Link>
-      <Link to='#' onClick={logOut}>Log out</Link>
+      <button type='button' onClick={logOut}style={buttonStyles} onMouseEnter={buttonHoverEffect} onMouseLeave={buttonHoverEffect} onMouseDown={buttonClickEffect} onMouseUp={buttonClickEffect}>Log out</button>
     </div>
   } else {
     sessionsLinks =
-    <div id='sessions-links' style={{display: 'flex', gap: '5px'}}>
+    <div id='sessions-links' style={{display: 'flex', gap: '15px'}}>
       <Link to='/login'>Log in</Link>
-      <Link to='/signup'>Sign up</Link>
+      <Link to='/signup' style={buttonStyles} onMouseEnter={buttonHoverEffect} onMouseLeave={buttonHoverEffect} onMouseDown={buttonClickEffect} onMouseUp={buttonClickEffect}>Sign up</Link>
     </div>
   }
 
